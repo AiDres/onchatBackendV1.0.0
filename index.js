3a56ce2e0d5f42c223883c6f1c3bed48b94e7a95
Join <1418145349@qq.com>
2019-11-19 01:50:11 +0800

    完善token生成

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const session = require('express-session');
 const pool = require('./pool.js');
-
+const md5 = require('md5');
 var app = express();
 app.listen(3000);
 app.use(express.static('public'));
@@ -35,10 +35,12 @@ app.use(cors({
   maxAge: 3600
 }))
 
+let token = 'md5-'+ new Date().getTime();
+console.log(md5(token));
 app.use(function (req, res, next) {
-	let resLogin = req.session.islogin;
+	let resLogin = req.session.security;
 	if (!resLogin) {
-	  req.session.islogin = {}
+	  req.session.security = {}
 	}
 	next()
 })
@@ -48,30 +50,48 @@ app.post('/onchat/login',(req,res)=>{
 	let upwd = req.body.pwd;
 	if(!email){res.send({code:-1,msg:'邮箱不能为空'});return;};
 	if(!upwd){res.send({code:-2,msg:'邮箱不能为空'});return;};
-	let sql = 'SELECT userid,uname,uAvatar,email,sex,fllowers,friends,tips,news FROM users WHERE email=? AND upwd=md5(?)';
+
+	// 查询是否存在用户
+	let sql = 'SELECT userid,uname,uAvatar,email,sex,fllowers,friends,tips,news,registerDate FROM users WHERE email=? AND upwd=md5(?)';
 	pool.query(sql,[email,upwd],(err,result)=>{	
 		if(err) throw err;
+
+		// 找到用户
 		if(result.length){
-			req.session.islogin['state']="true";
-			req.session.islogin['uid']=result[0].userid;
-			res.send({code:200,data:result});
+			// 生成token
+			let tokencode = 'md5-'+result[0].registerDate+Math.floor(Math.random() * Math.floor(new Date().getTime()));
+			req.session.security['tokenCode']=tokencode;
+
+			// 更新保存token至数据库
+			let sql = 'UPDATA users SET securitycode=? WHERE userid=?';
+			pool.query(sql,[tokencode,result[0].userid],(err,setResult)=>{
+				if(err) throw err;
+				if(result.affectedRows>0){
+
+					// 数据保存成功并将之前查询到的用户数据返回给客户端
+					res.send({code:200,data:result});
+				}else{
+					res.send({code:301,logid:tokencode,msg:'服务器异常，请将logid提供给管理员'})
+				}
+			});
+			
 		}else{
-			res.send({code:301,msg:"手机号或密码不匹配"});
+			res.send({code:301,msg:"邮箱或密码不匹配"});
 		}
 	});
 });
 
 // 判断是否是登录状态
 app.post('/onchat/hasLogin',(req,res)=>{
-	if(req.session.islogin && req.session.islogin.state){
-		let userInfo = {};
-		let sql = 'SELECT userid,uname,uAvatar,email,sex,fllowers,friends,tips,news FROM users WHERE userid=?';
-		pool.query(sql,[req.session.islogin.uid],(err,result)=>{
+	let session = req.session.security;
+	if(session && session.tokenCode && req.body.tokencode === session.tokenCode){
+		let sql = 'SELECT userid,uname,uAvatar,email,sex,fllowers,friends,tips,news FROM users WHERE securitycode=?';
+		pool.query(sql,[session.tokenCode],(err,result)=>{
 			if(err) throw err;
 			if(result.length){
 				res.send({code:305,msg:'已登录',data:result});
 			}else{
-				res.send({code:403,msg:'服务器异常',data:{}});
+				res.send({code:403,msg:'服务器异常，请将logid提供给管理员',logid:session.tokenCode});
 			}
 			
 		})
@@ -81,21 +101,33 @@ app.post('/onchat/hasLogin',(req,res)=>{
 	
 });
 
+function getUserid(params,session){
+	let uid = -1;
+	if(session && session.tokenCode && params.tokencode === session.tokenCode){
+		let sql = 'SELECT userid FROM users WHERE securitycode=?';
+		pool.query(sql,[params.tokencode],(err,result)=>{
+			if(err) throw err;
+			if(result.length){uid = result[0].userid;}
+		})
+	}
+	return uid;
+}
 
 // 获取动态
 app.post('/onchat/article',(req,res)=>{
-	if(req.session.islogin && req.session.islogin.state){
+	let session = req.session.security;
+	let uid = getUserid(req.body,session);
+	if(uid!=-1){
 		let userInfo = {};
 		let sql = 'SELECT userid,newsid,title,newslike,themeimage,newstime,content,spectator FROM ownnews WHERE userid=?';
-		console.log(req.session.islogin.uid,'article')
-		pool.query(sql,[req.session.islogin.uid],(err,result)=>{
+		pool.query(sql,[uid],(err,result)=>{
 			if(err) throw err;
 			
 			if(result.length){
 				console.log(result)
 				res.send({code:305,msg:'获取成功',data:result});
 			}else{
-				res.send({code:403,msg:'服务器异常',data:{}});
+				res.send({code:403,msg:'服务器异常',logid:session.tokenCode});
 			}
 			
 		})
