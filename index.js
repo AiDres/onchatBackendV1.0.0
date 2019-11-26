98daaa71c1e3e3dfa042f849432579f8958120f0
Join <1418145349@qq.com>
2019-11-26 22:59:58 +0800

    updata

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const session = require('express-session');
-const pool = require('./pool.js');
-const md5 = require('md5');
-const fs = require('fs');
+const userRouter = require('./routers/userRouter');
+const socialMatteRouter = require('./routers/sociMatterRouter');
+
+
 var app = express();
-app.listen(3000);
+app.listen(3000,()=>{
+	console.log('listen a port 3000')
+});
 app.use(express.static('public'));
 
 // 处理post请求
@@ -43,104 +46,6 @@ app.use(function (req, res, next) {
 	}
 	next()
 })
+app.use('/user',userRouter);
+app.use('/socialMatter',socialMatteRouter);
 
-app.post('/onchat/login',(req,res)=>{
-	let email = req.body.email;
-	let upwd = req.body.pwd;
-	if(!email){res.send({code:-1,msg:'邮箱不能为空'});return;};
-	if(!upwd){res.send({code:-2,msg:'密码不能为空'});return;};
-
-	// 查询是否存在用户
-	let sql = 'SELECT userid,uname,uAvatar,email,sex,fllowers,friends,tips,news,registerDate FROM users WHERE email=? AND upwd=md5(?)';
-	pool.query(sql,[email,upwd],(err,result)=>{	
-		if(err) throw err;
-
-		// 找到用户
-		if(result.length){
-			// 生成token
-			let tokencode = md5('md5-'+result[0].registerDate+Math.floor(Math.random() * Math.floor(new Date().getTime())));
-			req.session.security['tokenCode']=tokencode;
-
-			// 更新保存token至数据库
-			let sql = 'UPDATE users SET securitycode=? WHERE userid=?';
-			pool.query(sql,[tokencode,result[0].userid],(err,setResult)=>{
-				if(err) throw err;
-				if(setResult.affectedRows>0){
-
-					// 数据保存成功并将之前查询到的用户数据返回给客户端
-					res.send({code:200,data:result,tokencode:tokencode});
-				}else{
-					res.send({code:301,logid:tokencode,msg:'服务器异常，请将logid提供给管理员'})
-				}
-			});
-			
-		}else{
-			res.send({code:301,msg:"邮箱或密码不匹配"});
-		}
-	});
-});
-
-// 判断是否是登录状态
-app.post('/onchat/hasLogin',(req,res)=>{
-	let session = req.session.security;
-	if(session && session.tokenCode && req.body.tokencode === session.tokenCode){
-		let sql = 'SELECT userid,uname,uAvatar,email,sex,fllowers,friends,tips,news FROM users WHERE securitycode=?';
-		pool.query(sql,[session.tokenCode],(err,result)=>{
-			if(err) throw err;
-			if(result.length){
-				res.send({code:305,msg:'已登录',data:result});
-			}else{
-				res.send({code:403,msg:'服务器异常，请将logid提供给管理员',logid:session.tokenCode});
-			}
-			
-		})
-	}else{
-		res.send({code:404,msg:'未登录'});
-	}
-	
-});
-
-function getUserid(params,session,success){
-	let uid = -1;
-	if(session && session.tokenCode && params.tokencode === session.tokenCode){
-		let sql = 'SELECT userid FROM users WHERE securitycode=?';
-		pool.query(sql,[params.tokencode],(err,result)=>{
-			if(err) throw err;
-			if(result.length){
-				uid = result[0].userid;
-				success(uid);
-			}else{
-				success(uid);
-			}
-		})
-	}
-	
-}
-
-// 获取动态
-app.post('/onchat/article',(req,res)=>{
-	let session = req.session.security;
-	getUserid(req.body,session,(uid)=>{
-		if(uid!=-1){
-			let userInfo = {};
-			let sql = 'SELECT userid,newsid,title,newslike,themeimage,newstime,content,spectator FROM ownnews WHERE userid=?';
-			pool.query(sql,[uid],(err,result)=>{
-				if(err) throw err;
-				let data = 'logId:'+req.body.tokencode+','+'request:'+JSON.stringify(req.body)+',result:'+JSON.stringify(result);
-				fs.appendFile('logs.log',`${JSON.stringify(data)}\n`,(err,result)=>{
-					if(err) throw err;
-					console.log('日志已更新');
-				})
-				if(result.length){
-					res.send({code:305,msg:'获取成功',data:result});
-				}else{
-					res.send({code:403,msg:'服务器异常',logid:session.tokenCode});
-				}
-				
-			})
-		}else{
-			res.send({code:404,msg:'未登录'});
-		}
-	});
-	
-})
